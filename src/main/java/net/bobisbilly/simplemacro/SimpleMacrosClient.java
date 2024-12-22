504ea1ad0cd12508c4b5d1c5c8850c6582e58a47
BobisBilly <devindadudenz@gmail.com>
2024-12-23 09:18:40 +1300

    Literally everything

---
+++ src/main/java/net/bobisbilly/simplemacro/SimpleMacrosClient.java
@@ -0,0 +1,10 @@
+package net.bobisbilly.simplemacro;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SimpleMacrosClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/bobisbilly/simplemacro/SimpleMacrosDataGenerator.java
@@ -0,0 +1,11 @@
+package net.bobisbilly.simplemacro;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimpleMacrosDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
